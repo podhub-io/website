606f151e2909d16436d5f2480dccf7a61fd3428f
Jon Chen <bsd@voltaire.sh>
2015-06-06 03:14:42 -0700

    add first_name and last_name to profile

--- podhub/website/models.py
+++ podhub/website/models.py
@@ -27,12 +27,17 @@ class Profile(Base):
     username = db.Column(db.String(80), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     display_name = db.Column(db.String(80), index=True, unique=True)
+    first_name = db.Column(db.String(80), index=True)
+    last_name = db.Column(db.String(80), index=True)
     user_id = db.Column(UUID, db.ForeignKey('user.id'))
 
-    def __init__(self, username, email, display_name=None):
+    def __init__(self, username, email, first_name=None, last_name=None,
+                 display_name=None):
         self.username = username
         self.email = email
         if not display_name:
             self.display_name = username
         else:
             self.display_name = display_name
+        self.first_name = first_name
+        self.last_name = last_name
