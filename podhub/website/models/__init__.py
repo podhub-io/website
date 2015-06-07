17d3812137a4d1155a2de5c00c7e6654b1836281
Jon Chen <bsd@voltaire.sh>
2015-06-07 02:39:06 -0700

    add base db class

---
+++ podhub/website/models/__init__.py
@@ -0,0 +1,14 @@
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.func import now
+import sqlalchemy
+import uuid
+from podhub.website import db
+
+
+class Base(db.Model):
+    id = db.Column(UUID, default=lambda: str(uuid.uuid4()), primary_key=True)
+    created_at = db.Column(db.DateTime(), default=now())
+    updated_at = db.Column(
+        sqlalchemy.DateTime(), default=now(), onupdate=now())
+
+    __mapper_args__ = {'order_by': sqlalchemy.desc('updated_at')}
--- podhub/website/models/users.py
+++ podhub/website/models/users.py
@@ -1,17 +1,4 @@
-from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.func import now
-import sqlalchemy
-import uuid
-from . import db
-
-
-class Base(db.Model):
-    id = db.Column(UUID, default=lambda: str(uuid.uuid4()), primary_key=True)
-    created_at = db.Column(db.DateTime(), default=now())
-    updated_at = db.Column(
-        sqlalchemy.DateTime(), default=now(), onupdate=now())
-
-    __mapper_args__ = {'order_by': sqlalchemy.desc('updated_at')}
+from . import Base
 
 
 class User(Base):
