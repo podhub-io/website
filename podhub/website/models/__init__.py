61ab419a9fe15138c05c85e9c7b05125300907b5
Jon Chen <bsd@voltaire.sh>
2015-06-10 01:24:25 -0700

    fix imports

--- podhub/website/models/__init__.py
+++ podhub/website/models/__init__.py
@@ -1,15 +1,15 @@
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.func import now
 import sqlalchemy
 import uuid
-from podhub.website import db
+from .. import db
 
 
 class Base(db.Model):
     id = db.Column(UUID, default=lambda: str(uuid.uuid4()), primary_key=True)
-    created_at = db.Column(db.DateTime(), default=now())
+    created_at = db.Column(db.DateTime(), default=sqlalchemy.func.now())
     updated_at = db.Column(
-        sqlalchemy.DateTime(), default=now(), onupdate=now())
+        sqlalchemy.DateTime(), default=sqlalchemy.func.now(),
+        onupdate=sqlalchemy.func.now())
 
     __mapper_args__ = {'order_by': sqlalchemy.desc('updated_at')}
 
